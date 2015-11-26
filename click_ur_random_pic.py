52a375fd577fe082731506d2dffa7422647efa6f
Kajal Gupta <shriyanka26@gmail.com>
2015-11-26 22:57:53 +0530

    editing seconds

--- click_ur_random_pic.py
+++ click_ur_random_pic.py
@@ -10,7 +10,7 @@ while True:
 	command='vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path pics --scene-prefix %s --scene-format png vlc://quit --run-time=1'%(random.randint(1,10000))
 	cmd=subprocess.Popen([command],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,bufsize=0)
 	try:
-		time.sleep(3)
+		time.sleep(900)
 	except:
 		sys.exit("Existing")
 
