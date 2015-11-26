e943b2939e11b63ef8e37cb17c5fbfcf6d88ed50
shriyanka <shriyanka26@gmail.com>
2015-11-26 22:52:19 +0530

    Click your random photos every 15 minutes while you code to have your really random selfie :P

---
+++ click_ur_random_pic.py
@@ -0,0 +1,16 @@
+import subprocess
+import random
+import time
+import sys
+
+
+md=subprocess.Popen(['mkdir pics'],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+while True:
+	command='vlc -I dummy v4l2:///dev/video0 --video-filter scene --no-audio --scene-path pics --scene-prefix %s --scene-format png vlc://quit --run-time=1'%(random.randint(1,10000))
+	cmd=subprocess.Popen([command],shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,bufsize=0)
+	try:
+		time.sleep(3)
+	except:
+		sys.exit("Existing")
+
