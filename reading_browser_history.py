551a98e327710740458cd16a1e1891e01b255891
shriyanka <shriyanka26@gmail.com>
2016-04-10 19:07:04 +0530

    making changes

--- reading_browser_history.py
+++ reading_browser_history.py
@@ -1,17 +1,21 @@
-#for ubuntu users
+"""
+Project: Sentiment Analysis using Background History data
+Module 1: Reading the User History table and storing the links in History.txt
+Module 2: Opening each urls and fetching the raw text seperatly in each file as __url__name.txt
+Tools: Python, Urlfetch, Subprocess-for making comand line request
+Date: 4th Dec, 2015 
+"""
+
 import sys
 import subprocess
 import requests
-import xml.dom.minidom
-from xml.dom.minidom import parse, parseString
-from xml.dom import minidom
 import urlfetch
 
-username="kajal"  #your username here
-default_dir_name="hkjxuece.default"  # find the name of your directory and add it here
+username="kajal"
+default_dir_name="hkjxuece.default"
 
-#path="/home/%s/.mozilla/firefox/%s/"%(username,default_dir_name)
-#subprocess.Popen(['sqlite3 places.sqlite "select url from moz_places order by last_visit_date desc limit 30" > ~/Desktop/history.txt'],cwd=path,shell=True)
+path="/home/%s/.mozilla/firefox/%s/"%(username,default_dir_name)
+subprocess.Popen(['sqlite3 places.sqlite "select url from moz_places order by last_visit_date desc limit 30" > ~/Desktop/history.txt'],cwd=path,shell=True)
 
 f=open("/home/%s/Desktop/history.txt"%username)
 url=f.read().split('\n')
@@ -20,15 +24,6 @@ for u in url:
 	resp = urlfetch.fetch(u, method='GET')
 	source = resp.content
 	print source
-	#xml_resp = minidom.parseString(source)
-	#print xml_resp
-	#body  = xml_resp.getElementsByTagName('body')[0].childNodes[0].data
-	#print body
 	t=open("/home/%s/Desktop/%s.html"%(username,url.index(u)),'w')
 	t.write(source)
 	t.close()
-
-#parsing the browsing page one by one
-
-
-
